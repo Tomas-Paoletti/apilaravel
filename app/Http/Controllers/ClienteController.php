f7394c87f3f2b3aa050c7e9289820c1f2aaca5b4
Tomas-Paoletti <tomiagustin.tp@gmail.com>
2023-01-09 21:46:52 -0300

    first commit

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cliente;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ClienteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Cliente::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //dd($$request->all());
+        $cliente = new Cliente;
+        $cliente->nombres =$request->nombres;
+        $cliente->apellido =$request->apellido;
+        $cliente->save();//guarda los datos elegidos
+        return $cliente;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Cliente  $cliente
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cliente $cliente)
+    {
+
+        return $cliente;//$cliente es el id del cliente
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Cliente  $cliente
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cliente $cliente)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Cliente  $cliente
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cliente $cliente)
+    {
+        //
+    }
+}
